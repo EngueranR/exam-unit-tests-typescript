29d6564a2f6133c6943e6999a191182582cb527f
engueranraout <rt.engueran@gmail.com>
2024-07-25 14:49:09 +0200

    test: TDD add tests for isRangeAvailable

--- src/ex1/index.spec.ts
+++ src/ex1/index.spec.ts
@@ -1,3 +1,70 @@
+/* [4 points] écrire les tests correspondants à tous les cas que vous jugez pertinent de tester. */
+
+import { isRangeAvailable, DateRange } from "./index";
+
 describe("isRangeAvailable", () => {
-  // écrire les tests ici
+  it("Retourne true si la plage demandée est disponible", () => {
+    const selectedDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const availableDate: DateRange = {
+      startDate: new Date("2024-07-20"),
+      endDate: new Date("2024-08-04"),
+    };
+    const result = isRangeAvailable(selectedDate, availableDate);
+    expect(result).toBe(true);
+  });
+
+  it("Retourne false si la plage demandée n'est pas disponible", () => {
+    const selectedDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const availableDate: DateRange = {
+      startDate: new Date("2024-08-05"),
+      endDate: new Date("2024-08-10"),
+    };
+    const result = isRangeAvailable(selectedDate, availableDate);
+    expect(result).toBe(false);
+  });
+
+  it("Retourne true si la plage demandée est égale à la plage disponible", () => {
+    const selectedDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const availableDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const result = isRangeAvailable(selectedDate, availableDate);
+    expect(result).toBe(true);
+  });
+
+  it("Retourne false si la plage demandée est partiellement disponible", () => {
+    const selectedDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const availableDate: DateRange = {
+      startDate: new Date("2024-07-30"),
+      endDate: new Date("2024-08-05"),
+    };
+    const result = isRangeAvailable(selectedDate, availableDate);
+    expect(result).toBe(false);
+  });
+
+  it("Retourne false si la plage demandée est entièrement en dehors de la plage disponible", () => {
+    const selectedDate: DateRange = {
+      startDate: new Date("2024-07-25"),
+      endDate: new Date("2024-08-01"),
+    };
+    const availableDate: DateRange = {
+      startDate: new Date("2024-08-05"),
+      endDate: new Date("2024-08-10"),
+    };
+    const result = isRangeAvailable(selectedDate, availableDate);
+    expect(result).toBe(false);
+  });
 });
