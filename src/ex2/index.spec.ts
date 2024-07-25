e1910480aed15755ae894230eaaf3d77a15b64a0
engueranraout <rt.engueran@gmail.com>
2024-07-25 15:03:47 +0200

    feat: Add tests for submitOrder function and mock sendOrderEmail

--- src/ex2/index.spec.ts
+++ src/ex2/index.spec.ts
@@ -1,3 +1,34 @@
+/* 
+[3 points]
+Ecrire les tests correspondants à tous les cas que vous jugez 
+pertinent de tester. La fonction sendOrderEmail ne 
+doit pas être réellement appelée, elle doit 
+être remplacée par un bouchon (mock).
+*/
+
+import { submitOrder, Order } from "./index";
+import { sendOrderEmail } from "./lib/email";
+
+jest.mock("./lib/email");
+
 describe("submitOrder", () => {
-  // écrire les tests ici
+  it("Définir le paramètre isSubmitted de la commande à true, et exécute sendOrderEmail", () => {
+    const order: Order = {
+      id: "ORDER_1234",
+      isSubmitted: false,
+    };
+    submitOrder(order);
+    expect(order.isSubmitted).toBe(true);
+    expect(sendOrderEmail).toHaveBeenCalledWith(order.id);
+  });
+
+  it("Retourne une erreur si la commande est déjà envoyé", () => {
+    const order: Order = {
+      id: "ORDER_1234",
+      isSubmitted: true,
+    };
+
+    expect(() => submitOrder(order)).toThrow("La commande a déjà été soumise.");
+    expect(sendOrderEmail).not.toHaveBeenCalled();
+  });
 });
