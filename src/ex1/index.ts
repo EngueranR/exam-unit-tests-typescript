32c5fcf4328932818845a8eb7ca0b809de3df122
engueranraout <rt.engueran@gmail.com>
2024-07-25 14:54:58 +0200

    feat : isRangeAvailable function

--- src/ex1/index.ts
+++ src/ex1/index.ts
@@ -1,3 +1,13 @@
+/*
+[2 points] 
+Dans le fichier src/ex1/index.ts, 
+écrire l’implémentation de la fonction isRangeAvailable, 
+qui prend en argument une plage de dates demandées
+(avec une date de début et une date de fin) 
+et une plage de dates disponibles, et retourne true
+si la plage demandée est incluse dans la plage de dates disponibles, false sinon.
+*/
+
 export type DateRange = {
   startDate: Date;
   endDate: Date;
@@ -7,5 +17,13 @@ export function isRangeAvailable(
   requestedRange: DateRange,
   availableRange: DateRange
 ): boolean {
-  return false;
+  const { startDate: selectedStartDate, endDate: selectedEndDate } =
+    requestedRange;
+  const { startDate: availableStartDate, endDate: availableEndDate } =
+    availableRange;
+
+  return (
+    selectedStartDate >= availableStartDate &&
+    selectedEndDate <= availableEndDate
+  );
 }
